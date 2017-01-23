00f01e608f6bb259cbefbc0d6b3b1800a95d01bc
Peter Miklos <pt.miklos@gmail.com>
2017-01-23 15:49:29 +0000

    app.module updated, forms components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { BrowserModule }    from '@angular/platform-browser';
-import { NgModule }         from '@angular/core';
-import { FormsModule }      from '@angular/forms';
-import { HttpModule }       from '@angular/http';
-import { MaterialModule }   from '@angular/material';
+import { BrowserModule }                      from '@angular/platform-browser';
+import { NgModule }                           from '@angular/core';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { HttpModule }                         from '@angular/http';
+import { MaterialModule }                     from '@angular/material';
 import 'hammerjs';
 
-import { AppComponent }     from './app.component';
+import { AppComponent }                   from './app.component';
+import { DynamicFormComponent }           from './forms/dynamic-form.component';
+import { DynamicFormQuestionComponent }   from './forms/dynamic-form-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicFormComponent,
+    DynamicFormQuestionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     MaterialModule.forRoot()
   ],
