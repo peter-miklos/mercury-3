dc846310589c8d5d0df73ddfe2f98e346891c68f
Peter Miklos <pt.miklos@gmail.com>
2017-01-23 17:41:40 +0000

    dynamic-form-question.component files finalized

--- src/app/forms/dynamic-form-question.component.ts
+++ src/app/forms/dynamic-form-question.component.ts
@@ -1,7 +1,18 @@
-import { Component }      from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup }        from '@angular/forms';
+
+import { QuestionBase }     from './question-base';
 
 @Component({
+  moduleId: module.id,
   selector: 'dynamic-form-question',
-  template: ''
+  templateUrl: 'dynamic-form-question.component.html'
 })
-export class DynamicFormQuestionComponent {}
+
+export class DynamicFormQuestionComponent {
+  @Input() question: QuestionBase<any>;
+  @Input() form: FormGroup;
+  get isValid() {
+    return this.form.controls[this.question.key].valid;
+  }
+}
